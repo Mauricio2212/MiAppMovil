8fc2e2b2f905345a602419c844622d328c7df620
Mauricio del Valle PC <mauriciodelvallefarrera@gmail.com>
2023-05-15 06:53:08 -0600

    Rama de autenticación, fallo en login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { user } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { userInfo } from 'os';
+import { Observable } from 'rxjs';
 import { AdminService } from 'src/app/services/admin.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { FirestorageService } from 'src/app/services/firestorage.service';
@@ -13,6 +16,8 @@ import { User } from 'src/app/shared/user.interface';
 })
 export class LoginPage implements OnInit {
 
+
+
   constructor(public authSvc:AuthService, 
               public router:Router,
               public frStore:FirestoreService,
@@ -21,56 +26,21 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {}
   async onLogin(email, password){
-    /*try {
+    try {
       const user = await this.authSvc.login(email.value, password.value);
-      const admin = await this.admSvc.loginAdmin(email.value, password.value);
       //To do: Verificar si el usuario verificó su Email
       const isVerified = this.authSvc.isEmailVerified(user);
-      const isAdminVerified = true;
       if(isVerified){
         console.log('Verified-->', isVerified);
+      }else
+        console.log('Verified-->', isVerified);
         this.redirectUser(isVerified);
-      }else if (isAdminVerified){
-        console.log('Admin Verified-->', isAdminVerified);
-        this.redirectAdmin(isVerified);
-      }
-    } catch (error) {
+      } catch (error) {
       console.log('Error-->', error)
     }
   }
 
-  async onLoginGoogle(){
-    try {
-      const user = await this.authSvc.loginGoogle();
-      if(user){
-        //To do: Verivicar si el usuario verificó su Email
-        const isVerified = this.authSvc.isEmailVerified(user);
-        console.log('User-->', user);
-        console.log('Verified-->', isVerified);
-        this.redirectUser(isVerified);
-        const uid = await this.authSvc.getUid();
-        console.log('uid-->',uid);
-      }
-    } catch (error) {
-      console.log('Error-->', error);
-    }
-  }
 
-  async onLoginFacebook(){
-    try {
-      const user = await this.authSvc.loginFacebook();
-      if(user){
-        //To do: Verivicar si el usuario verificó su Email
-        const isVerified = this.authSvc.isEmailVerified(user);
-        console.log('User-->', user);
-        console.log('Verified-->', isVerified)
-        this.redirectUser(isVerified);
-        this.redirectAdmin(isVerified);
-      }
-    } catch (error) {
-      console.log('Error-->', error);
-    }
-  }
 
   //Método para redireccionar al usuario
   private redirectUser(isVerified:boolean){
@@ -82,16 +52,5 @@ export class LoginPage implements OnInit {
       this.router.navigate(['verify-email']);
     }
   }
-
-   //Método para redireccionar al admin
-   private redirectAdmin(isVerified:boolean){
-    if(isVerified){
-      //redireccionar a tab1
-      this.router.navigate(['home']);
-    }else{
-      //Enviar a página de verificación
-      this.router.navigate(['home']);
-    } */
-  }
- }
+}
 
